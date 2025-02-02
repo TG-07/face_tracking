396413f3cdc91cb43021a1747faad94240aa672f
Tanisha Gupta <tanisha@tanishas-mbp.wifi.local.cmu.edu>
2025-02-02 17:20:30 -0500

    minor changes

--- setup_environment.sh
+++ setup_environment.sh
@@ -7,13 +7,16 @@ ENV_NAME="face_tracking"
 echo "Creating Conda environment: $ENV_NAME"
 conda create --name $ENV_NAME python=3.8 -y
 
+# Ensure Conda is properly initialized
+echo "Initializing Conda..."
+eval "$(conda shell.bash hook)"
+
 # Activate the Conda environment
 echo "Activating Conda environment..."
 conda activate $ENV_NAME
 
 # Install required Python packages
 echo "Installing Python dependencies..."
-
 pip install face_recognition
 pip install opencv-contrib-python
 
